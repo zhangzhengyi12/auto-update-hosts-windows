5f5e25d0e962569bd3d84fe246df47b6a6dfea98
Laoliu <zhangzhengyi12@live.com>
2017-09-20 22:36:24 -0700

    firstcommit

---
+++ main.js
@@ -0,0 +1,57 @@
+const request = require("request");
+const fs = require("fs");
+const path = require("path");
+const winddowsPath = `C:${path.sep}Windows${path.sep}system32${path.sep}drivers${path.sep}etc`
+const downloadURI = "https://raw.githubusercontent.com/sy618/hosts/master/FQ";
+const zipName = "hosts"
+
+
+function downloadFilePromisify(uri, filename) {
+  return new Promise((resolve, reject) => {
+    let stream = fs.createWriteStream(filename)
+    request(uri)
+      .pipe(stream)
+      .on("close", () => {
+        console.log("下载成功")
+        resolve("download")
+      })
+  })
+}
+
+function unZipFilePromisify(filePath, unzipPath) {
+  return new Promise((resolve, reject) => {
+    let unzip = new admZip(filePath);
+    unzip.extractAllTo(unzipPath, true);
+    console.log("解压成功");
+    resolve("unzip");
+  });
+}
+
+function moveFilePromisify(filePath, toPath,fileName) {
+  return new Promise((resolve, reject) => {
+    let sourcePath = path.join(filePath,fileName)
+    let destPath = path.join(toPath,fileName)
+    let readHostsStream = fs.createReadStream(sourcePath)
+    let writeHostsStream = fs.createWriteStream(destPath)
+    readHostsStream.pipe(writeHostsStream)
+    console.log('完成....... ✔')
+    resolve('move')
+  })
+}
+
+
+
+
+
+function updateHostsFile() {
+  downloadFilePromisify(downloadURI, zipName)
+    .then((mess) => {
+      return moveFilePromisify('',winddowsPath,'hosts')
+    })  
+}
+
+updateHostsFile()
+
+
+
+
